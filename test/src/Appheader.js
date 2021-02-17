2b91c67e082c7c5fe5d807cc6b82e9c6695fea79
patis <arm1patis@gmail.com>
2021-02-17 12:24:21 +0700

    lan

---
+++ test/src/Appheader.js
@@ -0,0 +1,12 @@
+function Appheader() {
+    
+    return (
+       
+             <header className="App-header">
+             <img className ="header-logo" src ="/New folder (3)/1.jpg"/>
+    
+           
+          </header>
+          );
+          }
+    export default Appheader;
---
+++ test/src/items.js
@@ -0,0 +1,28 @@
+const items =[
+    {
+      title :"man",
+      thumbnaiUrl :"/New folder (3)/1.jpg"
+      
+    
+    },
+    {
+      title :"gril",
+      thumbnaiUrl :"/New folder (3)/2.jfif"
+      
+    
+    },
+    {
+      title :"boy",
+      thumbnaiUrl :"/New folder (3)/3.jpg"
+      
+    
+    },
+    {
+      title :"view",
+      thumbnaiUrl :"/New folder (3)/4.webp"
+      
+    
+    }
+  ];
+  export default items;
+
